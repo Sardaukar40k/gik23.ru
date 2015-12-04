d891795fc2696fc1d7cfd1378e67549d4e29f1e4
Alexey Maevskiy <mr.overlord2012@gmail.com>
2015-12-04 13:20:41 +0300

    gik23.ru - v.2.2.2 - небольшие правки в скрипт плавающего меню

--- wp-content/themes/maininvest/src/js/sticky-menu.js
+++ wp-content/themes/maininvest/src/js/sticky-menu.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-  var $menu = $("#menu");
+  var menu = $("#menu");
+  var position = menu.offset().top;
 
   $(window).scroll(function() {
-    if ( $(this).scrollTop() > 100 && $menu.hasClass("menu-default")) {
-      $menu.removeClass("menu-default").addClass("navbar-fixed-top menu-fixed");
-    } else if ( $(this).scrollTop() <= 100 && $menu.hasClass("navbar-fixed-top menu-fixed")) {
-      $menu.removeClass("navbar-fixed-top menu-fixed").addClass("menu-default");
+    if ( $(this).scrollTop() > position && menu.hasClass("menu-default")) {
+      menu.removeClass("menu-default").addClass("navbar-fixed-top menu-fixed");
+    } else if ( $(this).scrollTop() <= position && menu.hasClass("navbar-fixed-top menu-fixed")) {
+      menu.removeClass("navbar-fixed-top menu-fixed").addClass("menu-default");
     }
   });//scroll
 });
